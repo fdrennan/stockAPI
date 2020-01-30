4358dd24a75ac0c9992fd5c1dc3c29fcd734ea0f
Freddy Drennan <drennanfreddy@gmail.com>
2020-01-30 10:59:43 -0700

    update

--- R/stock.R
+++ R/stock.R
@@ -1,14 +1,20 @@
 #' @param stocks Tickers in JSON format
 #' @param DATA bool
 #' @export get_stocks
-get_stocks <- function(stocks = '["AAPL","DIA"]', DATA = FALSE) {
+get_stocks <- function(stocks = '["AAPL","DIA"]',
+                       DATA = FALSE,
+                       startDate = "2016-01-01",
+                       endDate = "2017-01-01") {
 
+  if (startDate >= endDate) {
+    stop('startDate must be less than endDate')
+  }
   stocks <- fromJSON(stocks)
 
   mult_stocks <- tq_get(stocks,
                         get  = "stock.prices",
-                        from = "2016-01-01",
-                        to   = "2017-01-01")
+                        from = startDate,
+                        to   = endDate)
 
   if(length(stocks) == 1) {
     mult_stocks$symbol = stocks
--- misc/plumber.R
+++ misc/plumber.R
@@ -2,7 +2,7 @@
 
 library(plumber)
 library(stockAPI)
-
+library(tictoc)
 #* @filter cors
 cors <- function(req, res) {
 
@@ -21,13 +21,105 @@ cors <- function(req, res) {
 
 #* @jpeg (width = 1000, height = 800)
 #* @param stocks  Stocks in JSON
+#* @param startDate  Stocks in JSON
+#* @param endDate  Stocks in JSON
 #* @get /get_stocks
-get_stocks <- function(stocks = '["AAPL"]') {
-  stockAPI::get_stocks(stocks = stocks, DATA = FALSE)
+get_stocks <- function(stocks = '["AAPL"]',
+                       startDate = '2019-01-01',
+                       endDate = '2020-01-01',
+                       DATA = FALSE) {
+
+  # Build the response object (list will be serialized as JSON)
+  response <- list(statusCode = 200,
+                   data = "",
+                   message = "Success!",
+                   console = list(
+                     args = list(
+                       stocks = stocks,
+                       DATA = DATA,
+                       startDate = startDate,
+                       endDate = endDate
+                     ),
+                     runtime = 0
+                   )
+  )
+
+
+  response <- tryCatch(
+    {
+
+      # Run the algorithm
+      tic()
+      response$data <- stockAPI::get_stocks(stocks = stocks,
+                                            DATA = DATA,
+                                            startDate = startDate,
+                                            endDate = endDate)
+      timer <- toc(quiet = T)
+      response$console$runtime <- as.numeric(timer$toc - timer$tic)
+
+      return(response)
+    },
+    error = function(err) {
+      response$statusCode <- 400
+      response$message <- paste(err)
+
+      return(response)
+    }
+  )
+
+  return(response)
+
+
 }
 
 #* @param stocks  Stocks in JSON
+#* @param startDate  Stocks in JSON
+#* @param endDate  Stocks in JSON
 #* @get /get_stocks_data
-get_stocks_data <- function(stocks = '["AAPL"]') {
-  stockAPI::get_stocks(stocks = stocks, DATA = TRUE)
+get_stocks_data <- function(stocks = '["AAPL"]',
+                       startDate = '2019-01-01',
+                       endDate = '2020-01-01',
+                       DATA = TRUE) {
+
+  # Build the response object (list will be serialized as JSON)
+  response <- list(statusCode = 200,
+                   data = "",
+                   message = "Success!",
+                   console = list(
+                     args = list(
+                       stocks = stocks,
+                       DATA = DATA,
+                       startDate = startDate,
+                       endDate = endDate
+                     ),
+                     runtime = 0
+                   )
+  )
+
+
+  response <- tryCatch(
+    {
+
+      # Run the algorithm
+      tic()
+      response$data <- stockAPI::get_stocks(stocks = stocks,
+                                            DATA = DATA,
+                                            startDate = startDate,
+                                            endDate = endDate)
+      timer <- toc(quiet = T)
+      response$console$runtime <- as.numeric(timer$toc - timer$tic)
+
+      return(response)
+    },
+    error = function(err) {
+      response$statusCode <- 400
+      response$message <- paste(err)
+
+      return(response)
+    }
+  )
+
+  return(response)
+
+
 }
